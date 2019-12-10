057e1e1e709b73d4819fe2dfb9cb355049b4d601
Koray S <koray.sels@gmail.com>
2019-12-10 15:28:55 +0100

    add browser platform to not break browser builds
    Signed-off-by: Koray S <koray.sels@gmail.com>

---
+++ src/browser/RollbarProxy.js
@@ -0,0 +1,20 @@
+/**
+ * Rollbar.js
+ *
+ * Cordova Rollbar plugin for version >= 3.0.0
+ *
+ * Copyright(c) Resgrid 2015 (http://resgrid.com)
+ */
+
+
+var Rollbar = {};
+
+Rollbar.init = function (successCallback, errorCallback) {
+  return successCallback()
+};
+
+Rollbar.simulateCrash = function (successCallback, errorCallback) {
+  return successCallback()
+};
+
+module.exports = Rollbar;
